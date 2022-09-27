80f62a47b25d74348fe82261fa1730296c8b8425
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2022-09-27 02:23:41 -0700

    Updated express-mongo-sanitize for a known patch of dryRun error, removed temp fix from #11

--- routes/signin.js
+++ routes/signin.js
@@ -24,7 +24,7 @@ router.post('/', function(req, res, next){
 		return;
 	}
 
-	var query = {email: {$regex: "^" + String(mongoSanitize.sanitize(body['email'], {dryRun : false})) + "$", $options: "i"}};
+	var query = {email: {$regex: "^" + String(mongoSanitize.sanitize(body['email'])) + "$", $options: "i"}};
 	var projection = {name: 1, club_id: 1, division_id: 1, access: 1};
 
 	app.db.collection("members").findOne(query, projection, function(err, member){
